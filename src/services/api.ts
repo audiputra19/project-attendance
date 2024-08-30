d74b7ebe25d974e66585697e3b13ecf2ea8bafd9
audi putra <audiputra19@gmail.com>
2024-08-30 10:01:25 +0700

    update

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { RootState } from '../store';
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:3001',
+    baseUrl: 'https://api-absensi-liart.vercel.app',
     prepareHeaders: (headers, {getState}) => {
         const token = (getState() as RootState).auth.token;
         if(token){
