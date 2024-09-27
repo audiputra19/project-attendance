257500cc88053ae78c99f24b600fd6c00e221435
audi putra <audiputra19@gmail.com>
2024-09-27 16:57:02 +0700

    update

--- src/Components/ToggleSwitch.tsx
+++ src/Components/ToggleSwitch.tsx
@@ -17,10 +17,9 @@ const ToggleSwitch: FC<ToggleSwitchProps> = ({ isChecked, onTonggle }) => {
         />
         <div className="w-11 h-6 bg-gray-300 rounded-full peer-checked:bg-color-base 
                         peer-focus:outline-none peer-checked:after:translate-x-full 
-                        peer-checked:after:border-white after:content-[''] 
-                        after:absolute after:top-0.5 after:left-[2px] after:bg-white 
-                        after:border-gray-300 after:border after:rounded-full 
-                        after:h-5 after:w-5 after:transition-all"></div>
+                        peer-checked:after:border-white after:content-[''] after:absolute 
+                        after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 
+                        after:border after:rounded-full after:h-5 after:w-5 after:transition-all"></div>
     </label>
   );
 };
