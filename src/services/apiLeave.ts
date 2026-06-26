fe6df5199ffcd412c861b2c410f332e9879fc49e
audiputra19 <audiputra19@gmail.com>
2026-06-26 12:56:58 +0700

    update api leave

--- src/services/apiLeave.ts
+++ src/services/apiLeave.ts
@@ -3,12 +3,12 @@ import { leaveRes, reportLeaveRes } from "../interfaces/leave";
 
 export const apiLeave = createApi({
     reducerPath: 'apiLeave',
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://api-absensi-liart.vercel.app' }),
+    // baseQuery: fetchBaseQuery({ baseUrl: 'https://api-absensi-liart.vercel.app' }),
     // baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000' }),
     // baseQuery: fetchBaseQuery({ baseUrl: 'http://103.163.161.201:5000' }),
-    // baseQuery: fetchBaseQuery({ 
-    //     baseUrl: 'https://app.sknmedical.co.id/skn/hrd/cuti/', 
-    // }),
+    baseQuery: fetchBaseQuery({ 
+        baseUrl: 'https://app.sknmedical.co.id/skn/hrd/cuti/', 
+    }),
     endpoints: build => ({
         postLeave: build.query<leaveRes, { nik: number | undefined, tahun: number }>({
             query: (data) => ({
